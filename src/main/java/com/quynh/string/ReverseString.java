05785e5c9b66037f55ba36e482cd1b4b17802bd4
Quinn Le <quynhnle135@gmail.com>
2022-12-08 11:08:28 -0500

    Did question 344. Reverse String

---
+++ src/main/java/com/quynh/string/ReverseString.java
@@ -0,0 +1,21 @@
+package com.quynh.string;
+public class ReverseString {
+    public static void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length - 1;
+        while (l < r) {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
+        for (char c : s) {
+            System.out.println(c);
+        }
+    }
+}
