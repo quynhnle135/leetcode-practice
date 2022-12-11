e239c96cb056f17c18f38fadeff6596ce2106f09
Quinn Le <quynhnle135@gmail.com>
2022-12-11 18:51:16 -0500

    Still tried to solve Revere Word In String problem

--- src/main/java/com/quynh/string/ReverseWordInString.java
+++ src/main/java/com/quynh/string/ReverseWordInString.java
@@ -1,5 +1,7 @@
 package com.quynh.string;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
 
 public class ReverseWordInString {
@@ -18,43 +20,52 @@ public class ReverseWordInString {
             }
         }
         stack.push(currentWord);
-        while (!stack.isEmpty()) {
-            result += stack.pop() + " ";
+        for (int i = 0; i < stack.size(); i++) {
+            String current = stack.pop();
+            if (current != " ") {
+                result += current + " ";
+            }
         }
         return result.trim();
     }
 
-//    public static String reverseWordInString(String s) {
-//        s = s.trim();
-//        Stack<String> stack = new Stack<>();
-//        int index = 0;
-//        String currentWord = "";
-//        while (index < s.length()) {
-//            if (s.charAt(index) == ' ') {
-//                stack.push(currentWord);
-//                while (s.charAt(index) == ' ') {
-//                    index++;
-//                }
-//            } else {
-//                currentWord += s.charAt(index);
-//                index++;
-//            }
-//        }
-//        stack.push(currentWord);
-//        String result = "";
-//        while (!stack.isEmpty()) {
-//            result += stack.pop() + " ";
-//        }
-//        return result.trim();
-//    }
+    public static String reverseWordInString(String s) {
+        String[] words = s.split("\\s");
+        String result = "";
+        String space = " ";
+        for (int i = words.length - 1; i >= 0; i--) {
+            if (words[i].equalsIgnoreCase(space)) {
+                continue;
+            }
+            System.out.println(words[i]);
+            result += words[i] + " ";
+        }
+        return result.trim();
+    }
 
 
     public static void main(String[] args) {
         String s1 = "the sky is blue";
 //        System.out.println(reverseWordInString(s1));
-        System.out.println(reverse(s1));
+//        System.out.println(reverse(s1));
         String s2 = "a good   example";
-//        System.out.println(reverseWordInString(s2));
-        System.out.println(reverse(s2));
+        System.out.println(reverseWordInString(s2));
+//        System.out.println(reverse(s2));
+
+//        String s = "hello";
+//        String res = "";
+//        for (int i = 0; i < s.length(); i++) {
+//            res += s.charAt(i);
+//        }
+//        System.out.println(res);
+        // learned about split()
+//        s2.trim();
+//        String[] s = s2.split(" ");
+//        for (String str : s) {
+//            System.out.println(str);
+//        }
+
+//        String s3 = "  hello anh bin ";
+//        System.out.println(s3.trim());
     }
 }
