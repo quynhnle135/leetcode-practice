aefb686a7810bb7bf09eeb2ec4d3ed6ac4587236
Quinn Le <quynhnle135@gmail.com>
2022-12-22 15:11:50 -0500

    Reviewed this Asteroids Collision question

--- src/main/java/com/quynh/medium/AsteroidCollision.java
+++ src/main/java/com/quynh/medium/AsteroidCollision.java
@@ -1,8 +1,6 @@
 package com.quynh.medium;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class AsteroidCollision {
     // positive: right, negative: left
@@ -16,33 +14,35 @@ public class AsteroidCollision {
                 while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(asteroids[i])) {
                     stack.pop();
                 }
+
                 if (stack.isEmpty() || stack.peek() < 0) {
                     stack.push(asteroids[i]);
-                } else if (stack.peek() == Math.abs(asteroids[i])) {
+                } else if(stack.peek() == -asteroids[i]) {
                     stack.pop();
                 }
             }
+        }
 
+        int[] res = new int[stack.size()];
+        for (int i = res.length - 1; i >= 0; i--) {
+            res[i] = stack.pop();
         }
-        int[] result = new int[stack.size()];
-        for (int i = result.length - 1; i >= 0; i--) {
-            result[i] = stack.pop();
-        }
-        return result;
+        return res;
     }
 
     public static void main(String[] args) {
-        int[] asteroids1 = {5, 10, -5};
-        int[] res1 = asteroidCollision(asteroids1);
-        for (int i : res1) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
+//        int[] asteroids1 = {5, 10, -5};
+//        int[] res1 = asteroidCollision(asteroids1);
+//        for (int i : res1) {
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
 
-        int[] asteroids2 = {10, 2, -5};
+        int[] asteroids2 = {5, 10, -5, -2, -12};
         int[] res2 = asteroidCollision(asteroids2);
         for (int i : res2) {
             System.out.print(i + " ");
         }
+
     }
 }
