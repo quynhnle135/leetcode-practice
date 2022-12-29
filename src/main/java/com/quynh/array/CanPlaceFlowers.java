ccbd93df51cec65b613107b4089f3821cec9fc4e
Quinn Le <quynhnle135@gmail.com>
2022-12-29 15:08:33 -0500

    Solved question Can Place Flowers

--- src/main/java/com/quynh/array/CanPlaceFlowers.java
+++ src/main/java/com/quynh/array/CanPlaceFlowers.java
@@ -4,13 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CanPlaceFlowers {
-    public static void main(String[] args) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int[] flowerbed = {1, 0, 0, 0, 1};
+    public static boolean canPlaceFlowers(int[] flowerbed, int k) {
+        int count = 0;
         for (int i = 0; i < flowerbed.length; i++) {
-            map.put(flowerbed[i], i);
+            if (flowerbed[i] == 0) {
+                boolean emptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean emptyRight = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+
+                if (emptyLeft && emptyRight) {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+            }
         }
-        System.out.println(map);
+        return count >= k;
+    }
+    public static void main(String[] args) {
+        int[] flowerbed = {1,0,0,0,0,0, 0};
+        int n = 3;
+        System.out.println(canPlaceFlowers(flowerbed, n));
 
     }
     // 0 1 0 1 0 1 -> false
---
+++ src/main/java/com/quynh/medium/TopKFrequentWords.java
@@ -0,0 +1,28 @@
+package com.quynh.medium;
+
+import java.util.*;
+
+public class TopKFrequentWords {
+//    public static List<String> findFrequentWords(String[] words, int k) {
+//        List<String> result = new ArrayList<>();
+//        Map<String, Integer> map = new HashMap<>();
+//        for (String word : words) {
+//            map.put(word, map.getOrDefault(word, 0) + 1);
+//        }
+//        Arrays.sort(words);
+//        for (String word : words) {
+//            if (map.get(word) == k) {
+//                result.add(word);
+//                map.put(word, map.get(word) - 1);
+//            }
+//        }
+//        return
+//    }
+//    public static void main(String[] args) {
+//        String[] words = {"i","love","leetcode","i","love","coding"};
+//        Arrays.sort(words);
+//        for (String w : words) {
+//            System.out.println(w);
+//        }
+//    }
+}
